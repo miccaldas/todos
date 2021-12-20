baef0960b6378c9569d3ef80b45f8c7684ee7286
mclds <mclds@protonmail.com>
2021-12-20 23:45:07 +0000

    Codeberg commit

--- todos/todo.py
+++ todos/todo.py
@@ -1,14 +1,14 @@
 #  Module to write a todo and create an associated cron job
 
-from crontab import CronTab
 import click
+from crontab import CronTab
 
 
 def todo():
 
-    texto = input(click.style(' What is yout to-do? » ', fg='green', bold=True))
+    texto = input(click.style(" What is yout to-do? » ", fg="green", bold=True))
 
-    cron = CronTab('mic')
+    cron = CronTab("mic")
     job = cron.new(command='/usr/bin/dunstify "' + texto + '"')
     job.minute.every(59)
     cron.write()
